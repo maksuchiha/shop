7ee115830f08642f461d6468d230242d9d4a8127
maksuchiha <73041165+maksuchiha@users.noreply.github.com>
2021-10-15 02:03:09 +0300

    fix bugs

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
 "use strict"
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.nav-menu-mob'),
-        menuItem = document.querySelectorAll('.nav-menu-mob .link'),
+    const menu = document.querySelector('.nav-menu'),
+        menuItem = document.querySelectorAll('.nav-menu__link'),
         hamburger = document.querySelector('.hamburger'),
         body = document.querySelector('.overf');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('nav-menu-mob_active');
+        menu.classList.toggle('nav-menu_active');
         body.classList.toggle('overf_active');
     });
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('nav-menu-mob_active');
+            menu.classList.toggle('nav-menu_active');
             body.classList.toggle('overf_active');
         })
     })
